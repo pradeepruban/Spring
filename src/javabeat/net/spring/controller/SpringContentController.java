88d336da58acfeaa41dc3e03286314391ea366ac
Pradeep <Pradeep_Thangaraj@syntelinc.com>
2015-03-20 18:01:36 +0530

    Create a new project

---
+++ src/javabeat/net/spring/controller/SpringContentController.java
@@ -0,0 +1,23 @@
+package javabeat.net.spring.controller;
+
+//import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Controller
+public class SpringContentController {
+	UserDetails userDetails;
+	//@RequestMapping(value="/springcontent",method=RequestMethod.GET,produces={"application/xml", "application/json"})
+    @ResponseStatus(HttpStatus.OK)
+	public @ResponseBody
+	UserDetails getUser() {
+		UserDetails userDetails = new UserDetails();
+		userDetails.setUserName("Pradeep");
+		userDetails.setEmailId("pradeep@gmail.com");
+		return userDetails;
+	}
+}
